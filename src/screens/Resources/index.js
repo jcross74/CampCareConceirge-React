713b25342b4fe2f4117cf743d09bff6af54539ce
Jason Cross <jason@theivy.studio>
2025-05-22 08:11:45 -0400

    Hero sizing

--- src/screens/Resources/index.js
+++ src/screens/Resources/index.js
@@ -42,11 +42,10 @@ const Resources = () => {
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography component="span">Accordion 1</Typography>
+          <Typography component="span">How do I find camps and activities in my area?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Use our customized search tool to find camps and activities based on location, age group, interests, and more.
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -55,11 +54,10 @@ const Resources = () => {
           aria-controls="panel2-content"
           id="panel2-header"
         >
-          <Typography component="span">Accordion 2</Typography>
+          <Typography component="span">How often is your camp information updated?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          We work closely with our camp/activity partners to ensure our listings are accurate and up-to-date.
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -68,11 +66,10 @@ const Resources = () => {
           aria-controls="panel2-content"
           id="panel2-header"
         >
-          <Typography component="span">Accordion 3</Typography>
+          <Typography component="span">Can I search for camps based on my child’s interests?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Yes! Our search tool allows you to filter by interests, activities, location by zip code, and special themes to ensure you find the perfect fit.
         </AccordionDetails>
       </Accordion>
   
