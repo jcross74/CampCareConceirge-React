67d9df7a125e4f14048dc8e695fd6b4b5326e3a3
Jason Cross <jason@theivy.studio>
2025-05-22 08:19:18 -0400

    Content

--- src/screens/Main/Provide/index.js
+++ src/screens/Main/Provide/index.js
@@ -8,20 +8,20 @@ const Provide = () => {
     return (
         <div className={styles.section}>
             <div className={styles.container}>
-            <div className={styles.provide_image}>
-                <img className={styles.fullWidth}
-                    src="/images/content/WhatWeProvide.png"
-                    alt="Kids doing science activity"
-                />
-            </div>
+                <div className={styles.provide_image}>
+                    <img className={styles.fullWidth}
+                        src="/images/content/WhatWeProvide.png"
+                        alt="Kids doing science activity"
+                    />
+                </div>
 
-            <div className={styles.provide_content}>
-                <h2 className={styles.title}>What we provide?</h2>
-                <p className={styles.description}>We’re committed to helping you find the perfect camp or activity for your child—quickly and with confidence. That’s why we focus exclusively on programs in Fairfax County, ensuring that every option we feature is local, relevant, and trusted. By narrowing our focus, we make it easier for you to discover high-quality camps that match your child’s interests and your family’s needs—without the hassle of sorting through options that aren’t a good fit.
-                </p>
-                <Link to="/about" className={cn("button", styles.button)}>Learn More</Link>
-            </div>
-        
+                <div className={styles.provide_content}>
+                    <h2 className={styles.title}>What we provide?</h2>
+                    <p className={styles.description}>We are dedicated to helping you quickly and confidently find the perfect camp or activity for your child. To achieve this, we focus exclusively on programs available in Fairfax County. This ensures that every option we present is local, relevant, and trustworthy. By narrowing our focus, we make it easier for you to discover high-quality camps that align with your child's interests and your family's needs, without the hassle of sifting through unsuitable options.
+                    </p>
+                    <Link to="/about" className={cn("button", styles.button)}>Learn More</Link>
+                </div>
+
             </div>
         </div>
     )
